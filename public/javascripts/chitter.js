c6bd988edf121cae04eac11571eb77aeb4bc4f73
selva-oscura <selva_oscura@yahoo.com>
2014-08-07 23:46:21 -0700

    added window unload handler to remove user from user list and users object, removed duplicate variables, implemented posting of messages to chat space

--- public/javascripts/chitter.js
+++ public/javascripts/chitter.js
@@ -2,42 +2,72 @@
 // alert('hello');
 
 $(document).ready(function(){
-  io = io.connect();
-  var user_count=0;
-  var me={};
-  var colour_wheel = ['blue', 'red', 'green', 'purple', 'brown', 'orange', 'black', 'gray'];
-  function returnHome(){
-    window.location.href='/';
-  }
-  $('#exit').on('click', function(){
-  	returnHome();
-  	io.emit('user_departure', {name:me.name, id: me.id});
-  });
-  io.emit('request_setup', {message:'nada'});
-  io.on('user_setup', function(data){
-  	console.log(data);
-  	console.log('current user:', data.current_user, 'chitterers:', data.chitterers);
-  	for(i in data.chitterers){
-  		if(data.chitterers[i].id!==data.current_user.id){
-		    var user_colour = colour_wheel[user_count%colour_wheel.length];
-		    $('#user_list').append('<div id="'+data.chitterers[i].id+'"class="active_user"><div class="user_color" style="background-color:'+user_colour+'"></div><h3 class="user_name" style="color:'+user_colour+'">'+data.chitterers[i].name+'</h3><div class="clear"></div></div>');
-  			user_count++;
-  		}
-  		else{
-  			$('#name_span').append(data.current_user.name);
-  		}
-  	}
-  	me={name: data.current_user.name, id: data.current_user.id};
-  	// console.log(data.current_user.name, data.current_user.id);
-  	// $('#user_list').append('<p>'+data.current_user.name+' '+data.current_user.id +'</p>');
-  });
-  io.on('user_arrived', function(data){
-    user_count++;
-    var user_colour = colour_wheel[user_count%colour_wheel.length-1];
-    $('#user_list').append('<div id="'+data.new_arrival_id+'"class="active_user"><div class="user_color" style="background-color:'+user_colour+'"></div><h3 class="user_name" style="color:'+user_colour+'">'+data.new_arrival_name+'</h3><div class="clear"></div></div>');    
-  });
-  io.on('user_departed', function(data){
-  	// console.log(data, data.id, data.name);
-  	$('#'+data.id).remove();
-  });
+	io = io.connect();
+	var user_count=0;
+	var me={};
+	var hasOwnProperty = Object.prototype.hasOwnProperty;
+	function isEmpty(obj) {
+		// null and undefined are "empty"
+		if (obj == null) return true;
+		// Assume if it has a length property with a non-zero value that that property is correct.
+		if (obj.length > 0)    return false;
+		if (obj.length === 0)  return true;
+		// Otherwise, does it have any properties of its own? 
+		// Note: this doesn't handle toString and valueOf enumeration bugs in IE < 9
+		for (var key in obj) {
+		    if (hasOwnProperty.call(obj, key)) return false;
+		}
+		return true;
+	}
+	function returnHome(){
+		window.location.href='/';
+	}
+	$('#exit').on('click', function(){
+		returnHome();
+		io.emit('user_departure', {name:me.name, id: me.id});
+	});
+	$(window).unload(function() {
+  		io.emit('user_departure', {name:me.name, id: me.id});
+	});
+	console.log('29',me);
+	io.emit('request_setup', {message:'nada'});
+	io.on('user_setup', function(data){
+		console.log('32',me);
+		if(isEmpty(data.current_user)){
+			returnHome();
+			io.emit('user_departure', {name:me.name, id: me.id});
+		};
+		console.log('current user:', data.current_user, 'chitterers:', data.chitterers);
+		for(i in data.chitterers){
+			if(data.chitterers[i].id!==data.current_user.id){
+		    // var user_colour = colour_wheel[user_count%colour_wheel.length];
+		    $('#user_list').append('<div id="'+data.chitterers[i].id+'"class="active_user"><div class="user_color" style="background-color:'+data.chitterers[i].colour+'"></div><h3 class="user_name" style="color:'+data.chitterers[i].colour+'">'+data.chitterers[i].name+'</h3><div class="clear"></div></div>');
+				user_count++;
+			}
+			else{
+				$('#name_span').append(data.current_user.name);
+			}
+		}
+		me={name: data.current_user.name, colour: data.current_user.colour, id: data.current_user.id};
+		// console.log(data.current_user.name, data.current_user.id);
+		// $('#user_list').append('<p>'+data.current_user.name+' '+data.current_user.id +'</p>');
+	});
+	io.on('user_arrived', function(data){
+	// user_count++;
+	// var user_colour = colour_wheel[user_count%colour_wheel.length-1];
+	$('#user_list').append('<div id="'+data.id+'"class="active_user"><div class="user_color" style="background-color:'+data.colour+'"></div><h3 class="user_name" style="color:'+data.colour+'">'+data.name+'</h3><div class="clear"></div></div>');    
+	});
+	io.on('user_departed', function(data){
+		// console.log(data, data.id, data.name);
+		$('#'+data.id).remove();
+	});
+	$('#form').submit(function(){
+	console.log('chitter_post', me.name, me.id, $('#message').val());
+	io.emit('chitter_post', {name: me.name, id: me.id, colour:me.colour, message:$('#message').val()});
+	return false;
+	});
+	io.on('chitter_posted', function(data){
+		console.log('chitter_posted', data.colour, data.name, data.message);
+	$('#chitter_space').prepend('<p style="color:'+data.colour+'"><strong>'+data.name+': </strong>'+data.message+'</p>');    
+	})
 })
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,10 @@
 module.exports = function Route(app){
-  var users={};
   var user={};
   var chitterers = [];
   var chitter = [];
+  var message={};
+  var user_count=0;
+  var colour_wheel = ['blue', 'red', 'green', 'purple', 'brown', 'orange', 'black', 'gray'];
   app.get('/', function(req, res){                 	// '/' index route   .get refers to the response to a get request (as opposed to post)
     res.render('index', {title: 'Chitter'});
   });
@@ -13,24 +15,31 @@ module.exports = function Route(app){
   	console.log('Connecting, baby!');
   });
   app.io.route('request_setup', function(req){
-    req.io.emit('user_setup', {current_user: user, users:users, chitterers:chitterers});   
+    req.io.emit('user_setup', {current_user: user, chitterers:chitterers});
+    user={};
   })
   app.io.route('user_departure', function(req){
-    console.log('user_departure', req.data.name, req.data.id);
     chitterers=chitterers.filter(function(el){
       return el.name !== req.data.name;
     });
     app.io.broadcast('user_departed', {id:req.data.id, name:req.data.name});
   })
+  app.io.route('chitter_post', function(req){
+    message={name:req.data.name, id:req.data.id, message:req.data.message, colour:req.data.colour};
+    chitter.push(message);
+    app.io.broadcast('chitter_posted', {name:req.data.name, message:req.data.message, colour:req.data.colour});
+  });
   app.post('/process', function(req, res){
     /* set some session data */
     req.session.post_info = req.body;
-    users[req.sessionID] = {name: req.body.name, id:req.sessionID};
-    user={name: req.body.name, id:req.sessionID};
+    var user_colour = colour_wheel[user_count%colour_wheel.length];
+    user_count++;
+    user={name: req.body.name, id:req.sessionID, colour: user_colour};
     chitterers.push(user);
+    // user={};
     req.session.save(function(){
       res.redirect('/chat');
-      app.io.broadcast('user_arrived', {new_arrival_name: req.body.name, new_arrival_id:req.sessionID , user_list:users});
+      app.io.broadcast('user_arrived', {name: req.body.name, id:req.sessionID, colour:user_colour});
     });
   });
 }
