f358717fa98ab11f6058619c5e8de5ba9b3c567f
selva-oscura <selva_oscura@yahoo.com>
2014-08-12 21:03:45 -0700

    added comments, removed redundant code, stopped overflow of user names in user list

--- public/javascripts/chitter.js
+++ public/javascripts/chitter.js
@@ -3,8 +3,9 @@
 
 $(document).ready(function(){
 	io = io.connect();
-	var user_count=0;
+	// stores user's id, name, colour information
 	var me={};
+	// function for checking if an object is empty
 	var hasOwnProperty = Object.prototype.hasOwnProperty;
 	function isEmpty(obj) {
 		// null and undefined are "empty"
@@ -19,80 +20,107 @@ $(document).ready(function(){
 		}
 		return true;
 	}
+
+	//redirect to home/landing/index page
 	function returnHome(){
 		window.location.href='/';
 	}
+
+	// on detecting user clicking exit button, redirect to hom/landing/index page and emit user_departure to server
 	$('#exit').on('click', function(){
 		returnHome();
 		io.emit('user_departure', {name:me.name, id: me.id});
 	});
+	// on detect user leaving page, emit user_departure to server
 	$(window).unload(function() {
   		io.emit('user_departure', {name:me.name, id: me.id});
 	});
+
+	// on arrival at chat.ejs, new arrival requests server for setup information
 	io.emit('request_setup', {message:'nada'});
+
+	// user_setup emit to new arrival by server triggers 
+	//	set-up of list of users and recent messages
+	//	saving of user's information
 	io.on('user_setup', function(data){
+		// redirect to landing page for sign-in if no current_user information is available (and, therefore, sent)
 		if(isEmpty(data.current_user)){
 			returnHome();
 			io.emit('user_departure', {name:me.name, id: me.id});
 		};
-		console.log('current user:', data.current_user, 'chitterers:', data.chitterers, 'chitter', data.chitter);
-		for(i in data.chitterers){
-			if(data.chitterers[i].id===data.current_user.id){
-				// $('#user_list').append('<h2>Me</h2>');
-				$('#user_list').append('<div id="'+data.chitterers[i].id+'"class="active_user"><div class="user_color" style="background-color:'+data.chitterers[i].colour+'"></div><h3 class="user_name" style="color:'+data.chitterers[i].colour+'">'+data.chitterers[i].name+'</h3><div class="clear"></div></div>');
-				user_count++;
-			}
-		}
+		// place div with user's name and colour in list of current users
+		$('#user_list').append('<div id="'+data.current_user.id+'"class="active_user"><div class="user_color" style="background-color:'+data.current_user.colour+'"></div><h3 class="user_name" style="color:'+data.current_user.colour+'">'+data.current_user.name+'</h3><div class="clear"></div></div>');
+		$('#name_span').append(data.current_user.name);
+		// append div with user's name and colour for all other current users
 		$('#user_list').append('<h2>Other Users</h2>');
 		for(i in data.chitterers){
 			if(data.chitterers[i].id!==data.current_user.id){
-		    // var user_colour = colour_wheel[user_count%colour_wheel.length];
 		    $('#user_list').append('<div id="'+data.chitterers[i].id+'"class="active_user"><div class="user_color" style="background-color:'+data.chitterers[i].colour+'"></div><h3 class="user_name" style="color:'+data.chitterers[i].colour+'">'+data.chitterers[i].name+'</h3><div class="clear"></div></div>');
-				user_count++;
-			}
-			else{
-				$('#name_span').append(data.current_user.name);
 			}
 		}
+		// appends most recent messages to message area (chitter_space)
 		for(i in data.chitter){
 			$('#chitter_space').append('<p style="color:'+data.chitter[i].colour+'"><strong>'+data.chitter[i].name+': </strong>'+data.chitter[i].message+'</p>'); 
 		}
+		//get total height of overflow element and pass to scrollTop to push slider to bottom of page.
+        var height = $('#chitter_space')[0].scrollHeight;
+		$('#chitter_space').scrollTop(height);
+		// saves new arrival's data to me object
 		me={name: data.current_user.name, colour: data.current_user.colour, id: data.current_user.id};
-		// console.log(data.current_user.name, data.current_user.id);
-		// $('#user_list').append('<p>'+data.current_user.name+' '+data.current_user.id +'</p>');
 	});
+
+	// user_arrived triggers 
+	//	appending div with new user's name and colour to list of users 
+	//	appending of announcement of user arrival announcement to messages area (chitter_space)
 	io.on('user_arrived', function(data){
-		// user_count++;
-		// var user_colour = colour_wheel[user_count%colour_wheel.length-1];
+		// announces user arrival in the chitter space
 		$('#chitter_space').append('<p style="color:'+data.colour+'">'+'['+data.name+' has entered the chat room]</p>'); 
+		// appends div with new arrival's name and colour in list of users
 		$('#user_list').append('<div id="'+data.id+'"class="active_user"><div class="user_color" style="background-color:'+data.colour+'"></div><h3 class="user_name" style="color:'+data.colour+'">'+data.name+'</h3><div class="clear"></div></div>');    
+        //get total height of overflow element and pass to scrollTop to push slider to bottom of page.
+        var height = $('#chitter_space')[0].scrollHeight;
+		$('#chitter_space').scrollTop(height);
 	});
+
+	// user_departed triggers 
+	//	removal of div with new user's name and colour to list of users 
+	//	appending of announcement of user departure announcement to messages area (chitter_space)
 	io.on('user_departed', function(data){
-		// console.log(data, data.id, data.name);
-		$('#chitter_space').append('<p style="color:'+data.colour+'">'+'['+data.name+' has left to go chitter in RL]</p>'); 
+		// announces user departure in the chitter space
+		$('#chitter_space').append('<p style="color:'+data.colour+'">'+'['+data.name+' has left to go chitter in RL]</p>');
+		// removes div with departing user's name and colour from list of users 
 		$('#'+data.id).remove();
+        //get total height of overflow element and pass to scrollTop to push slider to bottom of page.
+        var height = $('#chitter_space')[0].scrollHeight;
+		$('#chitter_space').scrollTop(height);
 	});
+
+	// submit chat message (emit chitter_post to server) by clicking submit button
 	$('#form').submit(function(){
-		// console.log('chitter_post', me.name, me.id, $('#message').val());
 		io.emit('chitter_post', {name: me.name, id: me.id, colour:me.colour, message:$('#message').val()});
 		return false;
 	});
+
+	// submit chat message (emit chitter_post to server) by hitting Enter
 	document.onkeydown = function(e){
 		var keyCode = (window.event) ? event.keyCode : e.keyCode;
 		if(keyCode===13){
 			$('#form').submit();
 		}
 	}
-	io.on('chitter_posted', function(data){
-		// console.log('chitter_posted', data.colour, data.name, data.message);
-		$('#chitter_space').append('<p style="color:'+data.colour+'"><strong>'+data.name+': </strong>'+data.message+'</p>');
-        //get total height of overflow element
-        var height = $('#chitter_space')[0].scrollHeight;
-		//pass total height to scrollTop to push slider to bottom of page.
-		$('#chitter_space').scrollTop(height);
-	})
+
+	// on receipt of a valid message, server emits to poster clear_form, triggering reset of chat message form
 	io.on('clear_form', function(){
-		// console.log('clear_form sent');
+		// clears message form's textarea
 		$('#form').closest('form').find("textarea").val("")
 	})
+
+	// on receipt of a valid message, server broadcasts to all chitter_posted, triggering reset of chat message form
+	io.on('chitter_posted', function(data){
+		// appends most recent message to message area (chitter_space)
+		$('#chitter_space').append('<p style="color:'+data.colour+'"><strong>'+data.name+': </strong>'+data.message+'</p>');
+        //get total height of overflow element and pass to scrollTop to push slider to bottom of page.
+        var height = $('#chitter_space')[0].scrollHeight;
+		$('#chitter_space').scrollTop(height);
+	})	
 })
--- routes/index.js
+++ routes/index.js
@@ -5,20 +5,56 @@ module.exports = function Route(app){
   var chitter = [];
   var user_count=0;
   var colour_wheel = ['blue', 'crimson', 'green','deeppink', 'indigo', 'maroon', 'darkblue', 'orangered', 'darkgreen','black', 'fuchsia', 'gray'];
-  app.get('/', function(req, res){                 	// '/' index route   .get refers to the response to a get request (as opposed to post)
+
+  // route for landing (index) page
+  app.get('/', function(req, res){
     res.render('index', {title: 'Chitter'});
   });
-  app.get('/chat', function(req, res){                 	// '/' index route   .get refers to the response to a get request (as opposed to post)
+
+  // routing for sign-in from landing page
+  //  creates a sessionID and assigns a colour to the user profile
+  //  redirects new arrival to landing page
+  //  broadcasts user_arrived to all users but new arrival
+  //    id, name, colour for new arrival
+  app.post('/process', function(req, res){
+    /* set some session data */
+    req.session.post_info = req.body;
+    var user_colour = colour_wheel[user_count%colour_wheel.length];
+    user_count++;
+    user={name: req.body.name, id:req.sessionID, colour: user_colour};
+    chitterers.push(user);
+    req.session.save(function(){
+      res.redirect('/chat');
+      app.io.broadcast('user_arrived', {name: req.body.name, id:req.sessionID, colour:user_colour});
+    });
+  });
+
+  // route for actual app (chat) page
+  app.get('/chat', function(req, res){
     res.render('chat', {title: 'Chitter'});
   });
+
+  // checking connection
   app.io.on('connection', function(req){
-  	console.log('Connecting, baby!');
+    console.log('Connecting, baby!');
   });
+
+  // request_setup (sent by most recent arrival)
+  // action: 
+  //  emits user_setup to most recent arrival
+  //    current_user (most recent arrival's id, name, colour for posts), 
+  //    chitterers (id, name, colour for all users), 
+  //    chitter (most recent posts with user name, colour, and message content)
   app.io.route('request_setup', function(req){
-    console.log(chitter);
     req.io.emit('user_setup', {current_user: user, chitterers:chitterers, chitter:chitter});
     user={};
   })
+
+  // user_departure (sent by most recent departure)
+  // action: 
+  //  broadcasts user_departure to all users 
+  //    id, name, and colour for most recent departure
+  //  removes most recent departure from chitterers (array of all users)
   app.io.route('user_departure', function(req){
     var colour;
     for(i in chitterers){
@@ -31,24 +67,21 @@ module.exports = function Route(app){
       return el.name !== req.data.name;
     });
   })
+
+  // chitter_post (sent by any current user)
+  // action:
+  //  if message isn't empty (length>0)
+  //    a message object is created with the most recent post (name, id, message, colour)
+  //    the message object is pushed into the chitter array (history of messages)
+  //    broadcasts chitter_posted with name, message, and colour information
+  //    emits clear_form to original sender a trigger to clear the message form
   app.io.route('chitter_post', function(req){
-    message={name:req.data.name, id:req.data.id, message:req.data.message, colour:req.data.colour};
-    chitter.push(message);
-    if(chitter.length>20){chitter.shift();};
-    app.io.broadcast('chitter_posted', {name:req.data.name, message:req.data.message, colour:req.data.colour});
-    req.io.emit('clear_form',{message:'clear_form'});
-  });
-  app.post('/process', function(req, res){
-    /* set some session data */
-    req.session.post_info = req.body;
-    var user_colour = colour_wheel[user_count%colour_wheel.length];
-    user_count++;
-    user={name: req.body.name, id:req.sessionID, colour: user_colour};
-    chitterers.push(user);
-    // user={};
-    req.session.save(function(){
-      res.redirect('/chat');
-      app.io.broadcast('user_arrived', {name: req.body.name, id:req.sessionID, colour:user_colour});
-    });
+    if(req.data.message.length>0){    
+      message={name:req.data.name, id:req.data.id, message:req.data.message, colour:req.data.colour};
+      chitter.push(message);
+      if(chitter.length>20){chitter.shift();};
+      app.io.broadcast('chitter_posted', {name:req.data.name, message:req.data.message, colour:req.data.colour});
+      req.io.emit('clear_form',{message:'clear_form'});
+    }
   });
 }
