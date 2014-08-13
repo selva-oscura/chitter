940169e36cad241e60c03585ac9dbe212bf710c8
selva-oscura <selva_oscura@yahoo.com>
2014-08-12 19:28:15 -0700

    added user arrival/departure announcements, made layout responsive, added scrolling to messages area, and added recent comments on load

--- public/javascripts/chitter.js
+++ public/javascripts/chitter.js
@@ -29,15 +29,21 @@ $(document).ready(function(){
 	$(window).unload(function() {
   		io.emit('user_departure', {name:me.name, id: me.id});
 	});
-	console.log('29',me);
 	io.emit('request_setup', {message:'nada'});
 	io.on('user_setup', function(data){
-		console.log('32',me);
 		if(isEmpty(data.current_user)){
 			returnHome();
 			io.emit('user_departure', {name:me.name, id: me.id});
 		};
-		console.log('current user:', data.current_user, 'chitterers:', data.chitterers);
+		console.log('current user:', data.current_user, 'chitterers:', data.chitterers, 'chitter', data.chitter);
+		for(i in data.chitterers){
+			if(data.chitterers[i].id===data.current_user.id){
+				// $('#user_list').append('<h2>Me</h2>');
+				$('#user_list').append('<div id="'+data.chitterers[i].id+'"class="active_user"><div class="user_color" style="background-color:'+data.chitterers[i].colour+'"></div><h3 class="user_name" style="color:'+data.chitterers[i].colour+'">'+data.chitterers[i].name+'</h3><div class="clear"></div></div>');
+				user_count++;
+			}
+		}
+		$('#user_list').append('<h2>Other Users</h2>');
 		for(i in data.chitterers){
 			if(data.chitterers[i].id!==data.current_user.id){
 		    // var user_colour = colour_wheel[user_count%colour_wheel.length];
@@ -48,26 +54,45 @@ $(document).ready(function(){
 				$('#name_span').append(data.current_user.name);
 			}
 		}
+		for(i in data.chitter){
+			$('#chitter_space').append('<p style="color:'+data.chitter[i].colour+'"><strong>'+data.chitter[i].name+': </strong>'+data.chitter[i].message+'</p>'); 
+		}
 		me={name: data.current_user.name, colour: data.current_user.colour, id: data.current_user.id};
 		// console.log(data.current_user.name, data.current_user.id);
 		// $('#user_list').append('<p>'+data.current_user.name+' '+data.current_user.id +'</p>');
 	});
 	io.on('user_arrived', function(data){
-	// user_count++;
-	// var user_colour = colour_wheel[user_count%colour_wheel.length-1];
-	$('#user_list').append('<div id="'+data.id+'"class="active_user"><div class="user_color" style="background-color:'+data.colour+'"></div><h3 class="user_name" style="color:'+data.colour+'">'+data.name+'</h3><div class="clear"></div></div>');    
+		// user_count++;
+		// var user_colour = colour_wheel[user_count%colour_wheel.length-1];
+		$('#chitter_space').append('<p style="color:'+data.colour+'">'+'['+data.name+' has entered the chat room]</p>'); 
+		$('#user_list').append('<div id="'+data.id+'"class="active_user"><div class="user_color" style="background-color:'+data.colour+'"></div><h3 class="user_name" style="color:'+data.colour+'">'+data.name+'</h3><div class="clear"></div></div>');    
 	});
 	io.on('user_departed', function(data){
 		// console.log(data, data.id, data.name);
+		$('#chitter_space').append('<p style="color:'+data.colour+'">'+'['+data.name+' has left to go chitter in RL]</p>'); 
 		$('#'+data.id).remove();
 	});
 	$('#form').submit(function(){
-	console.log('chitter_post', me.name, me.id, $('#message').val());
-	io.emit('chitter_post', {name: me.name, id: me.id, colour:me.colour, message:$('#message').val()});
-	return false;
+		// console.log('chitter_post', me.name, me.id, $('#message').val());
+		io.emit('chitter_post', {name: me.name, id: me.id, colour:me.colour, message:$('#message').val()});
+		return false;
 	});
+	document.onkeydown = function(e){
+		var keyCode = (window.event) ? event.keyCode : e.keyCode;
+		if(keyCode===13){
+			$('#form').submit();
+		}
+	}
 	io.on('chitter_posted', function(data){
-		console.log('chitter_posted', data.colour, data.name, data.message);
-	$('#chitter_space').prepend('<p style="color:'+data.colour+'"><strong>'+data.name+': </strong>'+data.message+'</p>');    
+		// console.log('chitter_posted', data.colour, data.name, data.message);
+		$('#chitter_space').append('<p style="color:'+data.colour+'"><strong>'+data.name+': </strong>'+data.message+'</p>');
+        //get total height of overflow element
+        var height = $('#chitter_space')[0].scrollHeight;
+		//pass total height to scrollTop to push slider to bottom of page.
+		$('#chitter_space').scrollTop(height);
+	})
+	io.on('clear_form', function(){
+		// console.log('clear_form sent');
+		$('#form').closest('form').find("textarea").val("")
 	})
 })
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,10 @@
 module.exports = function Route(app){
   var user={};
   var chitterers = [];
-  var chitter = [];
   var message={};
+  var chitter = [];
   var user_count=0;
-  var colour_wheel = ['blue', 'red', 'green', 'purple', 'brown', 'orange', 'black', 'gray'];
+  var colour_wheel = ['blue', 'crimson', 'green','deeppink', 'indigo', 'maroon', 'darkblue', 'orangered', 'darkgreen','black', 'fuchsia', 'gray'];
   app.get('/', function(req, res){                 	// '/' index route   .get refers to the response to a get request (as opposed to post)
     res.render('index', {title: 'Chitter'});
   });
@@ -15,19 +15,28 @@ module.exports = function Route(app){
   	console.log('Connecting, baby!');
   });
   app.io.route('request_setup', function(req){
-    req.io.emit('user_setup', {current_user: user, chitterers:chitterers});
+    console.log(chitter);
+    req.io.emit('user_setup', {current_user: user, chitterers:chitterers, chitter:chitter});
     user={};
   })
   app.io.route('user_departure', function(req){
+    var colour;
+    for(i in chitterers){
+      if(chitterers[i].id===req.data.id){
+        colour=chitterers[i].colour;
+        app.io.broadcast('user_departed', {id:req.data.id, name:req.data.name, colour:colour});
+      }
+    }
     chitterers=chitterers.filter(function(el){
       return el.name !== req.data.name;
     });
-    app.io.broadcast('user_departed', {id:req.data.id, name:req.data.name});
   })
   app.io.route('chitter_post', function(req){
     message={name:req.data.name, id:req.data.id, message:req.data.message, colour:req.data.colour};
     chitter.push(message);
+    if(chitter.length>20){chitter.shift();};
     app.io.broadcast('chitter_posted', {name:req.data.name, message:req.data.message, colour:req.data.colour});
+    req.io.emit('clear_form',{message:'clear_form'});
   });
   app.post('/process', function(req, res){
     /* set some session data */
